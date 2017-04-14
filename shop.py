adcf510025bab0e14c548ce1af1b8dc73304ffaf
RAFAEL <rafaellichen@gmail.com>
2017-04-13 20:19:06 -0400

    fully operational shopping cart and checkout system

--- shop.py
+++ shop.py
@@ -17,11 +17,8 @@ def check_quantity(input):
         messagebox.showinfo("", "Please enter valid integer")
         return False
 
-def order_summary():
-    pass
-
 def get_list(a):
-    did_list = [int(x) for x in a]
+    did_list = [int(float(x)) for x in a]
     count = Counter(did_list)
     return count.most_common()
 
@@ -49,13 +46,83 @@ def write_cart(uid,cart):
         cart_str = ""
         for e in cart:
             cart_str+=str(e)+","
-        cart_str=cart_str[:-1]
+        cart_str=str(cart_str[:-1])
         cart_database = pandas.read_csv("data/users.csv")
-        cart_database.set_value(uid, "cart", cart_str)
+        cart_database.loc[cart_database["uid"]==uid, "cart"]=cart_str
         cart_database.to_csv("data/users.csv", index=False)
 
 def get_cart(uid):
     cart_database = pandas.read_csv("data/users.csv")
     cart_database = cart_database.loc[cart_database["uid"]==uid]["cart"].values
-    temp = cart_database[0].split(",")
+    temp = []
+    try:
+        if not numpy.isnan(cart_database[0]):
+            try:
+                temp = cart_database[0].split(",")
+            except AttributeError:
+                temp = [cart_database[0]]
+    except TypeError:
+        temp = cart_database[0].split(",")
+    print(type(temp))
     return temp
+
+def checkout_balance(level, total, uid):
+    if level != 1 and level != 2:
+        return False
+    data_file = pandas.read_csv("data/users.csv")
+    balance = (data_file.loc[data_file["uid"]==uid]["balance"].values)[0]
+    if balance >= total:
+        if level == 2:
+            result = messagebox.askyesno("Confirm payment","Discounted price: "+str(int((total*0.9))))
+            if result:
+                data_file.loc[data_file["uid"]==uid, "balance"]=balance-int((total*0.9))
+                data_file.loc[data_file["uid"]==uid, "cart"] = numpy.nan
+                data_file.to_csv("data/users.csv", index=False)
+                return True
+        if level == 1:
+            result = messagebox.askyesno("Confirm payment","Final price: "+str(int((total))))
+            if result:
+                print(balance-total)
+                data_file.loc[data_file["uid"]==uid, "balance"]=balance-total
+                data_file.loc[data_file["uid"]==uid, "cart"] = numpy.nan
+                data_file.to_csv("data/users.csv", index=False)
+                return True
+    else:
+        messagebox.showinfo("","Not enough balance")
+    return False
+
+def write_order(uid, cart):
+    data_file = pandas.read_csv("data/order.csv")
+    ddid = data_file["ddid"].values.tolist()
+    order = data_file["order"].values.tolist()
+    destination = data_file["destination"].values.tolist()
+    status = data_file["status"].values.tolist()
+    uid_data = data_file["uid"].values.tolist()
+    rate = data_file["rate"].values.tolist()
+    rated = data_file["rated"].values.tolist()
+    ddid.append(ddid[-1]+1)
+    cart_str = ""
+    for e in cart:
+        cart_str+=str(int(e))+","
+    cart_str=cart_str[:-1]
+    order.append(cart_str)
+    destination.append(numpy.nan)
+    status.append(-1)
+    uid_data.append(uid)
+    rate.append(0)
+    rated.append(0)
+    order_database = pandas.DataFrame({"ddid": ddid,
+                                        "order": order,
+                                        "destination": destination,
+                                        "status": status,
+                                        "uid": uid_data,
+                                        "rate": rate,
+                                        "rated": rated})
+    order_database = order_database[["ddid",
+                                    "order",
+                                    "destination",
+                                    "status",
+                                    "uid",
+                                    "rate",
+                                    "rated"]]
+    order_database.to_csv("data/order.csv", index=False)
